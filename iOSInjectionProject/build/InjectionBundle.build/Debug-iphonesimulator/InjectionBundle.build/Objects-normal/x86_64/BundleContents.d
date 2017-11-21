04c3902ddcffd6b3799f71f0918cf42f291995eb
GrayLand <languilin1109@vip.qq.com>
2017-11-21 14:56:41 +0800

    remove Injection

--- iOSInjectionProject/build/InjectionBundle.build/Debug-iphonesimulator/InjectionBundle.build/Objects-normal/x86_64/BundleContents.d
+++
@@ -1,6 +0,0 @@
-dependencies: \
-  /Users/languilin/Desktop/WorkSpacePrivate/GitHubGrayLand/GLEncryptManager/iOSInjectionProject/InjectionBundle-Prefix.pch \
-  /Users/languilin/Desktop/WorkSpacePrivate/GitHubGrayLand/GLEncryptManager/iOSInjectionProject/BundleContents.m \
-  /tmp/injectionforxcode/BundleInjection.h \
-  /tmp/injectionforxcode/BundleInterface.h \
-  /Users/languilin/Desktop/WorkSpacePrivate/GitHubGrayLand/GLEncryptManager/iOSInjectionProject/BundleContents.h
